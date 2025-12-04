37328e5d99de41d7af7c12256aadd276e55aec32
sathwiksrinivasak <sathwiksrinivasak@gmail.com>
2025-12-04 12:00:16 -0700

    Add files via upload

---
+++ my_favourites.py
@@ -0,0 +1,21 @@
+#Assignment1 - Program1 - my_favourites.py
+
+# Description:       The module contains functions that return my fav things 
+# Purpose:           Assignment 1
+# Developed By:      Sathwik Srinivasa Kadukuntla (837-935-176)
+#Date created :      01 Sept 2025
+#Date last modified: 01 Sept 2025
+
+def get_favourite_car(): #gives my fav car
+    return 'Land Cruiser'
+
+def get_favourite_quote():# gives my fav quote
+    return 'Be White or Black but never Gray'
+
+def get_favourite_movie(): #gives my fav movie
+    return 'Sapta Saagaralu Dhaati'
+
+print(f'{get_favourite_car()}')
+print(f'{get_favourite_quote()}')
+print(f'{get_favourite_movie()}')
+    
---
+++ program1.py
@@ -0,0 +1,18 @@
+#Assignment1 - Program1 - program1.py
+
+# Description:       This program displays my fav things by calling functions from my_favourites
+# Purpose:           Assignment 1
+# Developed By:      Sathwik Srinivasa Kadukuntla (837-935-176)
+#Date created :      01 Sept 2025
+#Date last modified: 01 Sept 2025
+
+import my_favourites as fav     #importing my_favourites program to this program
+
+print("Here are Sathwik Srinivasa Kadukuntla's favourite things - ")
+print (f"Sathwik's fav car is : {fav.get_favourite_car()}")#prints my fav car
+print (f"Sathwik's fav quote is : {fav.get_favourite_quote()}")#prints my fav quote
+print (f"Sathwik's fav movie is : {fav.get_favourite_movie()}")#prints my fav movie
+
+
+
+
